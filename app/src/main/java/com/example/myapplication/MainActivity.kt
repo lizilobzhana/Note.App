1f944cb7a84573857c63ee2dcf85351f42df4f42
Lizi Lobzhanidze <lobzhanidze03@gmail.com>
2022-01-22 04:24:39 +0400

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+
+        val navView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val controller = findNavController(R.id.nav_host_main_fragment)
+        navView.setupWithNavController(controller)
+
     }
 }
